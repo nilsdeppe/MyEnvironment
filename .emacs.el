88f0191da40d30895f2c65ae06c6d78cebf9519d
Nils Deppe <nils@nilsdeppe.com>
2024-04-14 00:13:35 -0400

    Emacs add support for lsp-booster

--- .emacs.el
+++ .emacs.el
@@ -1598,6 +1598,43 @@ Please set my:ycmd-server-command appropriately in ~/.emacs.el.\n"
     (local-set-key (kbd "C-c y o") 'lsp-restart-workspace))
   (local-set-key (kbd "C-c y c") 'lsp-disconnect)
   (local-set-key (kbd "C-c y f") 'lsp-format-region)
+
+  ;; Add emacs-lsp-booster support
+  (when (not (executable-find "emacs-lsp-booster"))
+      (warn "\nWARNING: Could not find the emacs-lsp-booster executable. It "
+            "is recommended you install emacs-lsp-booster."))
+
+  (defun lsp-booster--advice-json-parse (old-fn &rest args)
+    "Try to parse bytecode instead of json."
+    (or
+     (when (equal (following-char) ?#)
+       (let ((bytecode (read (current-buffer))))
+         (when (byte-code-function-p bytecode)
+           (funcall bytecode))))
+     (apply old-fn args)))
+  (advice-add (if (progn (require 'json)
+                         (fboundp 'json-parse-buffer))
+                  'json-parse-buffer
+                'json-read)
+              :around
+              #'lsp-booster--advice-json-parse)
+
+  (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+      (if (and (not test?)  ;; for check lsp-server-present?
+               ;; see lsp-resolve-final-command, it would add extra shell
+               ;; wrapper
+               (not (file-remote-p default-directory))
+               lsp-use-plists
+               (not (functionp 'json-rpc-connection))  ;; native json-rpc
+               (executable-find "emacs-lsp-booster"))
+          (progn
+            (message "Using emacs-lsp-booster for %s!" orig-result)
+            (cons "emacs-lsp-booster" orig-result))
+        orig-result)))
+  (advice-add
+   'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
