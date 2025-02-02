7903f53bfeaa75810e7ec410495bb7e878b1e691
Nils Deppe <nils@nilsdeppe.com>
2025-02-02 13:37:40 -0500

    Emacs: Filter unfixable warnings

--- .emacs.el
+++ .emacs.el
@@ -186,6 +186,26 @@ compilation."
 (defvar my:ws-butler-global-exempt-modes
   '(markdown-mode ein:notebook-multilang-mode))
 
+;; List of regex that hide warning messages from being printed.
+;;
+;; Sometimes there's nothing that can be done other than modify
+;; an upstream package to silence a warning. So we just filter
+;; those.
+(defvar my:warning-suppress-regex
+  '(".*Unknown notification: semgrep/rulesRefreshed")
+  )
+(defun my:display-warning-filter (type message &optional level buffer-name)
+  "Filter warnings based on a list of regular expressions."
+  (catch 'exit
+    (dolist (regexp my:warning-suppress-regex)
+      (when (string-match-p regexp message)
+        (throw 'exit nil))
+      )
+    (throw 'exit t)
+    ))
+(add-function :before-while (symbol-function 'display-warning)
+              #'my:display-warning-filter)
+
 ;; TEX is installed in a different location on macOS
 (when (string-equal system-type "darwin")
   (setenv "PATH" (concat (getenv "PATH") ":/Library/TeX/texbin/"))
